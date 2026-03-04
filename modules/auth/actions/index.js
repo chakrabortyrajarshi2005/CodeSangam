9be285474f94cea8847d68425338cd27ce289e86
ChakrabortyRajarshi2005 <rajarshi29032005@gmail.com>
2026-03-04 13:29:05 +0530

    add User Onboarding with Prisma - Defining Schema & Server Actions

---
+++ modules/auth/actions/index.js
@@ -0,0 +1,53 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+
+export const userOnBoard = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) {
+      return {
+        success: false,
+        error: "User is not authenticated",
+      };
+    }
+
+    const { id, firstName, lastName, imageUrl, emailAddresses } = user;
+
+    const email = emailAddresses[0]?.emailAddress || "";
+
+    const newUser = await db.user.upsert({
+      where: {
+        clerkId: id,
+      },
+      update: {
+        firstName: firstName || null,
+        lastName: lastName || null,
+        imageUrl: imageUrl || null,
+        email,
+      },
+      create: {
+        clerkId: id,
+        firstName: firstName || null,
+        lastName: lastName || null,
+        imageUrl: imageUrl || null,
+        email,
+      },
+    });
+
+    return {
+      success: true,
+      user: newUser,
+      message: "New user onboarded and inserted into DB",
+    };
+  } catch (error) {
+    console.log("Error in user onboarding:", error);
+
+    return {
+      success: false,
+      error: "Failed to onboard user",
+    };
+  }
+};
