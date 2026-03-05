ad20b93041aa62a8b0f4cdff9b39315e05a64388
ChakrabortyRajarshi2005 <rajarshi29032005@gmail.com>
2026-03-05 19:33:50 +0530

    changed componenyts/ui/mode-toggler.jsx

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -8,7 +8,7 @@ const RootLayout = async ({ children }) => {
     <main className="flex flex-col min-h-screen max-h-screen">
       <Navbar userRole={userRole} />
       <div className="flex-1 flex flex-col px-4 pb-4">
-        <div className="absolute inset-0 -z-10 h-full w-full bg-neutral-50 dark:bg-neutral-950 bg-[radial-gradient(circle_at_1px_1px,rgba(0,0,0,0.035)_1px,transparent_0)] dark:bg-[radial-gradient(circle_at_1px_1px,rgba(255,255,255,0.06)_1px,transparent_0)] bg-size-[22px_22px]" />
+        <div className="absolute inset-0 -z-10 h-full w-full bg-neutral-50 dark:bg-neutral-800 bg-[radial-gradient(circle_at_1px_1px,rgba(0,0,0,0.035)_1px,transparent_0)] dark:bg-[radial-gradient(circle_at_1px_1px,rgba(255,255,255,0.06)_1px,transparent_0)] bg-size-[22px_22px]" />
         {children}
       </div>
     </main>
